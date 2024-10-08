a1e0e06605434703d1dbf50bb50456d6219aeee5
suman sorav <suman sorav>
2024-10-08 22:24:54 +0530

    replace word

--- afctor.py
+++
@@ -1,5 +0,0 @@
-n = int(input("enter the no : "))
-product = 1 
-for i in range (1 , n+1):
-    product *= i
-    print(product)
--- pyTuturiol/module.py
+++
@@ -1,4 +0,0 @@
-import pyjokes
-# comment use forwad slash
-jokes = pyjokes.get_joke()
-print(jokes) 
